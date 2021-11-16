c390f7faab59edb3344a38166a84580577e9135d
Alan Cortes <ing.ti.upmh@gmail.com>
2021-11-15 23:07:08 -0600

    Crear un nuevo registro POST en base de datos

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { RespuestaPosts } from '../interfaces/interfaces';
+import { UsuarioService } from './usuario.service';
 
 const URL = environment.url;
 
@@ -12,7 +13,8 @@ export class PostsService {
 
   paginaPost = 0;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private usuarioService: UsuarioService) { }
 
   getPosts(pull: boolean = false){
 
@@ -22,6 +24,19 @@ export class PostsService {
     this.paginaPost ++;
 
     return this.http.get<RespuestaPosts>(`${URL}/posts/?pagina=${this.paginaPost}`);
+
+  }
+
+  crearPost(post) {
+
+    const headers = new HttpHeaders({
+      'x-token': this.usuarioService.token
+    });
+
+    this.http.post(`${ URL }/posts`, post, {headers}).subscribe(resp => {
+      console.log(resp);
+
+    });
 
   }
 
